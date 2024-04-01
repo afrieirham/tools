68e0141f5dd774356c0d3b1a89955a28c325847c
Afrie Irham <afrieirham.work@gmail.com>
2024-04-01 23:36:53 +0800

    add seohead generator

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex flex-col w-full p-6 mx-auto max-w-screen-sm">
+    <main className="flex flex-col w-full max-w-screen-sm p-6 mx-auto">
       <h1 className="text-xl font-bold">
         tools by{" "}
         <a className="hover:underline" href="https://afrieirham.com">
@@ -20,6 +20,14 @@ export default function Home() {
           </li>
           <li className="list-disc list-inside hover:underline">
             <Link href="/can-i-embed">can i embed? ↗</Link>
+          </li>
+          <li className="list-disc list-inside hover:underline">
+            <Link href="/seohead-generator">
+              <code className="px-2 py-1 text-sm bg-gray-100 rounded">
+                &lt;SEOHead/&gt;
+              </code>{" "}
+              generator ↗
+            </Link>
           </li>
         </ul>
       </div>
---
+++ src/pages/seohead-generator.tsx
@@ -0,0 +1,136 @@
+import { useState } from "react";
+
+function SeoHeadGenerator() {
+  const initialCode = `import Head from "next/head";
+  
+function SEOHead({
+  title,
+  description,
+  path,
+  ogPath = "/og.png",
+}: {
+  title: string;
+  description: string;
+  path: string;
+  ogPath: string;
+}) {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="robots" content="index,follow" />
+      <link rel="canonical" href="https://<domain_placeholder>" />
+
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+
+      {/* Facebook Meta Tags */}
+      <meta property="og:site_name" content={title} />
+      <meta property="og:url" content={\`https://<domain_placeholder>$\{path}\`} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={\`https://<domain_placeholder>$\{ogPath}\`} />
+
+      {/* Twitter Meta Tags */}
+      <meta name="twitter:creator" content="<twitter_placeholder>" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="<domain_placeholder>" />
+      <meta property="twitter:url" content={\`https://<domain_placeholder>$\{path}\`} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={\`https://<domain_placeholder>$\{ogPath}\`} />
+
+      {/* Meta Tags Generated via ${window.location} */}
+    </Head>
+  );
+}
+
+export default SEOHead;`;
+
+  const usageCode = `<SEOHead
+  title="My website title"
+  description="My website description"
+  path="/"
+  ogPath="/og.png"
+/>`;
+
+  const [domain, setDomain] = useState("afrieirham.com");
+  const [twitter, setTwitter] = useState("@afrieirham_");
+
+  return (
+    <div className="flex flex-col items-center w-full p-4 space-y-8 sm:p-8">
+      <div className="self-start">
+        <a href="/" className="hover:underline">
+          ← back to main page
+        </a>
+      </div>
+      <div className="max-w-sm mx-auto space-y-2">
+        <h1 className="text-center">
+          Next.js{" "}
+          <code className="px-2 py-1 text-sm bg-gray-100 rounded">
+            &lt;SEOHead/&gt;
+          </code>{" "}
+          Generator
+        </h1>
+        <input
+          placeholder="domain name"
+          value={domain}
+          onChange={(e) => setDomain(e.target.value)}
+          className="text-[12px] w-full rounded border border-gray-300 py-1 px-2 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2"
+        />
+        <input
+          placeholder="twitter username"
+          value={twitter}
+          onChange={(e) => setTwitter(e.target.value)}
+          className="text-[12px] w-full rounded border border-gray-300 py-1 px-2 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2"
+        />
+      </div>
+
+      <div className="w-full max-w-screen-lg mx-auto space-y-2 ">
+        <div className="flex justify-between">
+          <p className="text-base">Component</p>
+        </div>
+
+        <CodeBlock>
+          {initialCode
+            .replaceAll("<domain_placeholder>", domain)
+            .replaceAll("<twitter_placeholder>", twitter)}
+        </CodeBlock>
+      </div>
+
+      <div className="w-full max-w-screen-lg mx-auto space-y-2 ">
+        <div className="flex justify-between">
+          <p className="text-base">Usage</p>
+        </div>
+
+        <CodeBlock>{usageCode}</CodeBlock>
+      </div>
+    </div>
+  );
+}
+
+function CodeBlock({ children }: { children: React.ReactNode }) {
+  const [copying, setCopying] = useState(false);
+  return (
+    <div className="relative w-full p-4 text-sm border rounded-lg sm:p-6 bg-gray-50">
+      <button
+        onClick={() => {
+          setCopying(true);
+          navigator.clipboard.writeText(children as string);
+          setTimeout(() => {
+            setCopying(false);
+          }, 1000);
+        }}
+        className="absolute top-0 right-0 px-4 py-1 m-4 text-sm bg-gray-200 rounded-md hover:bg-gray-300"
+      >
+        {copying ? "copied!" : "copy"}
+      </button>
+      <div className="w-full overflow-scroll whitespace-pre">
+        <code>{children}</code>
+      </div>
+    </div>
+  );
+}
+
+export default SeoHeadGenerator;
